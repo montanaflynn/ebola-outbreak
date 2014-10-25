6b53398c6bf901aa9ef7155d01ad8658cd6db2a0
Montana Flynn <montana@montanaflynn.me>
2014-10-25 06:23:36 -0700

    Avoid errors when deleting data

--- test.js
+++ test.js
@@ -1,56 +1,60 @@
 var fs = require('fs')
 var ebola = require('./index.js')
 
-fs.unlinkSync("./data/cases.json")
+fs.unlink(__dirname + '/data/cases.json', runTests)
 
-// Return all the confirmed cases
-ebola.cases(function(err, output){
-	console.log("Testing returning all confirmed cases")
-	if (err) 
-		return console.log("\033[31m", "Test Failed", "\033[0m \n")
+function runTests() {
 
-	if (typeof output === "object") {
-		console.log("\033[32m", "Test Passed", "\033[0m \n")
-	} else {
-		console.log("\033[31m", "Test Failed", "\033[0m \n")
-	}
-})
+  // Return all the confirmed cases
+  ebola.cases(function(err, output){
+    console.log("Testing returning all confirmed cases")
+    if (err) 
+      return console.log("\033[31m", "Test Failed", "\033[0m \n")
 
-// Return confirmed cases plus 5 projections with the 'latest' model:
-ebola.project(5, 'latest' ,function(err, output){
-	console.log("Testing confirmed cases plus 5 projections with the 'latest' model")
-	if (err) 
-		return console.log("\033[31m", "Test Failed", "\033[0m \n")
+    if (typeof output === "object") {
+      console.log("\033[32m", "Test Passed", "\033[0m \n")
+    } else {
+      console.log("\033[31m", "Test Failed", "\033[0m \n")
+    }
+  })
 
-	if (typeof output === "object") {
-		console.log("\033[32m", "Test Passed", "\033[0m \n")
-	} else {
-		console.log("\033[31m", "Test Failed", "\033[0m \n")
-	}
-})
+  // Return confirmed cases plus 5 projections with the 'latest' model:
+  ebola.project(5, 'latest' ,function(err, output){
+    console.log("Testing confirmed cases plus 5 projections with the 'latest' model")
+    if (err) 
+      return console.log("\033[31m", "Test Failed", "\033[0m \n")
 
-// Return confirmed cases plus 5 projections with the 'average' model:
-ebola.project(5, 'average' ,function(err, output){
-	console.log("Testing confirmed cases plus 5 projections with the 'average' model")
-	if (err) 
-		return console.log("\033[31m", "Test Failed", "\033[0m \n")
+    if (typeof output === "object") {
+      console.log("\033[32m", "Test Passed", "\033[0m \n")
+    } else {
+      console.log("\033[31m", "Test Failed", "\033[0m \n")
+    }
+  })
 
-	if (typeof output === "object") {
-		console.log("\033[32m", "Test Passed", "\033[0m \n")
-	} else {
-		console.log("\033[31m", "Test Failed", "\033[0m \n")
-	}
-})
+  // Return confirmed cases plus 5 projections with the 'average' model:
+  ebola.project(5, 'average' ,function(err, output){
+    console.log("Testing confirmed cases plus 5 projections with the 'average' model")
+    if (err) 
+      return console.log("\033[31m", "Test Failed", "\033[0m \n")
 
-// Update data in './data/cases.json'
-ebola.update(function(err, output){
-	console.log("Testing updating the data")
-	if (err) 
-		return console.log("\033[31m", "Test Failed", "\033[0m \n")
-	
-	if (typeof output === "object") {
-		console.log("\033[32m", "Test Passed", "\033[0m \n")
-	} else {
-		console.log("\033[31m", "Test Failed", "\033[0m \n")
-	}
-})
+    if (typeof output === "object") {
+      console.log("\033[32m", "Test Passed", "\033[0m \n")
+    } else {
+      console.log("\033[31m", "Test Failed", "\033[0m \n")
+    }
+  })
+
+  // Update data in './data/cases.json'
+  ebola.update(function(err, output){
+    console.log("Testing updating the data")
+    if (err) 
+      return console.log("\033[31m", "Test Failed", "\033[0m \n")
+    
+    if (typeof output === "object") {
+      console.log("\033[32m", "Test Passed", "\033[0m \n")
+    } else {
+      console.log("\033[31m", "Test Failed", "\033[0m \n")
+    }
+  })
+
+}
