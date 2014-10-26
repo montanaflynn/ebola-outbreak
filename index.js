29a172ce1f716491537b0d68a4e4f96ff8d0987e
Montana Flynn <montana@montanaflynn.me>
2014-10-26 05:43:42 -0700

    Refactor code, better projection modeling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
 var unirest = require('unirest')
 var parse = require('csv-parse')
+var regression = require('regression')
 
 var dataFile = __dirname + '/data/cases.json'
 
@@ -10,60 +11,12 @@ module.exports = {
       callback(false, require(dataFile))
       return
     }
-    unirest
-    .get('http://healthmap.org/ebola/csv/allnone.csv')
-    .end(function(res) {
-      parse(res.body, function(err, output) {
-        if (typeof output !== "object") return callback(true, res)
-        output.shift()
-        var data = []
-        var last = 0
-        for (var i = 0; i < output.length; i++) {
-          if (output[i][1] != 'X') {
-            var cases = parseInt(output[i][1])
-            data.push({
-              date: new Date(output[i][0]).toISOString(),
-              cases: cases,
-              status: 'confirmed'
-            })
-          }
-        }
-        var output = mungeData(data, 'cases')
-        saveData(output, function() {
-          callback(err, output)
-        })
-      })
-    })
+    getData(callback)
   },
   project: function(distance, callback) {
     this.cases(function(err, data) {
       if (err) return callback(err, false)
-      var data = data.map(function(datum) {
-        var originalData = parseInt(datum.cases['new'])
-        datum['cases'] = originalData
-        return datum
-      })      
-      var cleanData = data.map(function(datum) {
-        return datum.cases
-      })
-      var guesses = []
-      while (distance > 0) {
-        var last = cleanData[data.length - 1]
-        var growth = growthRate(cleanData)
-        var guess = Math.floor(last * growth)
-        cleanData.push(guess)
-        var newDate = new Date(data[data.length - 1]['date'])
-        newDate.setHours(24 * 18)
-        data.push({
-          date: newDate.toISOString(),
-          cases: guess,
-          status: 'projection'
-        })
-        distance--
-      }
-      // console.log(data)
-      var output = mungeData(data, 'cases')
-      console.log(output)
+      var output = projectData(distance, data)
       callback(err, output)
     })
   },
@@ -73,12 +26,6 @@ module.exports = {
       callback(false, response)
     })
   }
-}
-
-function growthRate(data) {
-  var current = data[data.length - 1]
-  var previous = data[data.length - 2]
-  return (current / previous)
 }
 
 function checkForDataUpdate() {
@@ -97,24 +44,95 @@ function checkForDataUpdate() {
   return false
 }
 
+function getData(callback) {
+  unirest
+  .get('http://healthmap.org/ebola/csv/allnone.csv')
+  .end(function(res) {
+    parse(res.body, function(err, output) {
+      if (typeof output !== "object") return callback(true, res)
+      var data = cleanData(output)
+      saveData(data, function() {
+        callback(err, data)
+      })
+    })
+  })
+}
+
+function parseData(input) {
+  return data
+}
+
+function cleanData(input) {
+  input.shift()
+  var data = []
+  var last = 0
+  for (var i = 0; i < input.length; i++) {
+    if (input[i][1] != 'X') {
+      var cases = parseInt(input[i][1])
+      data.push({
+        date: new Date(input[i][0]).toISOString(),
+        cases: cases,
+        status: 'confirmed'
+      })
+    }
+  }
+  return data
+}
+
+function projectData(distance, data) {
+
+  // Get last date
+  var lastDate = new Date(data[data.length-1]['date'])
+
+  // Add to the inital data
+  while (distance > 0) {
+
+    // Create new date
+    lastDate.setHours(24 * 18)
+
+    // Add projected placeholder
+    data.push({ 
+      date: lastDate.toISOString(),
+      cases: null,
+      status: 'projected'
+    })
+
+    distance--
+  }
+
+  // Get the data in usable form
+  var regressionData = []
+  for (var i = 0; i < data.length; i++) {
+    var datum
+    if (!data[i]) {
+      datum = null
+    } else {
+      datum = data[i].cases
+    }
+
+    regressionData.push([i,datum])
+  }
+
+  // Build the projection model
+  var model = regression('exponential', regressionData).points
+  var model = model.map(function(data) {
+    data[1] = Math.round(data[1])
+    return data
+  })   
+
+  // Add back to original data
+  for (var i = 0; i < data.length; i++) {
+    var datum = data[i]
+    if (!datum.cases) datum.cases = model[i][1]
+  }
+
+  // Voila
+  return data
+}
+
 function saveData(data, callback) {
   fs.writeFile(dataFile, JSON.stringify(data, null, 2), function(err) {
     if (err) throw err
     callback()
   })
-}
-
-function mungeData(source, property) {
-  var sum = 0;
-  return source.map(function(current) {
-    var cumulative = sum + current[property];
-    sum += current[property];
-    var datoid = {
-      "new" : current[property],
-      "total" : cumulative
-    }
-    delete current[property]
-    current[property] = datoid
-    return current;
-  });  
 }
